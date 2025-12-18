d8f13400dabd2731603f625547f3d704a68f6683
soroushmoradi <soroushmoradi13.sm@gmail.com>
2025-12-18 16:18:35 +0000

    reconfig tailwind

--- src/Components/Filters.jsx
+++ src/Components/Filters.jsx
@@ -48,13 +48,13 @@ export default function Filters({
         value={search}
         onChange={(e) => setSearch(e.target.value)}
         placeholder="جستجو..."
-        className="border p-2 outline-none border rounded-full focus:ring-none rtl"
+        className="border p-2 outline-none border rounded-full focus:ring-none focus:border-blue-500 rtl"
       />
 
       <select
         value={category}
         onChange={(e) => setCategory(e.target.value)}
-        className="focus:outline-none p-2 border-2 border-gray-500 rounded-md"
+        className="focus:outline-none p-2 border-2 border-gray-500 rounded-md border-blue-500"
       >
         <option value="">همه دسته‌ها</option>
         {categoryOptions.map((c) => (
@@ -67,7 +67,7 @@ export default function Filters({
       <select
         value={sort}
         onChange={(e) => setSort(e.target.value)}
-        className="focus:outline-none p-2 border-2 border-gray-500 rounded-md"
+        className="focus:outline-none p-2 border-2 border-gray-500 rounded-md border-blue-500"
       >
         {sortOptions.map((s) => (
           <option key={s.value} value={s.value}>
